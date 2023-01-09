36c74536bf8fc8a191953fc4e217f46a887bce6a
Thomas <112832846+hudsonthomas55@users.noreply.github.com>
2023-01-09 15:55:50 -0500

    Created txt file
    Created txt file with initial restaurants, to be updated later

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # ---------------------------- Imports ---------------------------- #
 import random
 
-
 # ---------------------------- Begin Script ---------------------------- #
 
 # Set 'user' to False so that it will rerun code at end
@@ -14,16 +13,25 @@ while not user:
     # Connect input to meal
     if user_lower == "breakfast":
         # Still need to connect data in breakfast column.
+        with open("breakfast.txt") as b_menu:
+            b_menu.read()
+
         print("Still in process, you requested a breakfast restaurant.")
         user = False
 
     elif user_lower == "lunch":
         # still need to connect data in lunch column
+        with open("lunch.txt") as l_menu:
+            l_menu.read()
+
         print("Still in process, you requested a lunch restaurant.")
         user = False
 
     elif user_lower == "dinner":
         # Still need to connect data in dinner column
+        with open("dinner.txt") as d_menu:
+            d_menu.read()
+
         print("Still in process, you selected a dinner restaurant")
         user = False
 
