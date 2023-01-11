3a8c46396e388f1129f2dab764ae391ad6cf2622
Thomas <112832846+hudsonthomas55@users.noreply.github.com>
2023-01-10 19:37:22 -0500

    Large update
    Created menu to view and add restaurants to the list. Allows for user to type 'exit' to return to main menu. Need to create 'delete' menu

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-# ---------------------------- Imports ---------------------------- #
+# ----------------------------------------------- Imports ----------------------------------------------- #
 import random
 
-# ---------------------------- Begin Script ---------------------------- #
+# --------------------------------------------- Begin Script ----------------------------------------------#
 
 # Set 'user' to False so that it will rerun code at end
 user = False
 while not user:
     # Ask user input for which meal is requested
-    user = input("Are you hungry for 'Breakfast', 'Lunch', or 'Dinner'? \n")
-    user_lower = user.lower()
+    user = input("Are you hungry for 'Breakfast', 'Lunch', or 'Dinner'? \nIf you wish to view/change the options type "
+                 "'View' or 'Change.'\n").lower()
 
-    # Connect input to meal
-    if user_lower == "breakfast":
+    # --------------------------------------- Run for meal options --------------------------------------- #
+    if user == "breakfast":
         def breakfast_choice():
             with open("breakfast.txt", 'r') as b_menu:
                 restaurants = b_menu.readlines()
@@ -20,7 +20,7 @@ while not user:
             return random_rest
         breakfast_choice()
         user = False
-    elif user_lower == "lunch":
+    elif user == "lunch":
         def lunch_choice():
             with open("lunch.txt", 'r') as l_menu:
                 restaurants = l_menu.readlines()
@@ -29,7 +29,7 @@ while not user:
             return random_rest
         lunch_choice()
         user = False
-    elif user_lower == "dinner":
+    elif user == "dinner":
         def dinner_choice():
             with open("dinner.txt", 'r') as d_menu:
                 restaurants = d_menu.readlines()
@@ -39,6 +39,98 @@ while not user:
         dinner_choice()
         user = False
 
+    # ------------------------------------------ View Meal Options ------------------------------------------ #
+    elif user == "view":
+        def view_options():
+            global user
+            option_view = input("Do you want to view the options for 'Breakfast', 'Lunch', or 'Dinner'? \nEnter "
+                                "'exit' to return to main menu \n").lower()
+            if option_view == "breakfast":
+                with open("breakfast.txt") as b_menu:
+                    print(b_menu.read())
+                    view_options()
+            elif option_view == "lunch":
+                with open("lunch.txt") as l_menu:
+                    print(l_menu.read())
+                    view_options()
+            elif option_view == "dinner":
+                with open("dinner.txt") as d_menu:
+                    print(d_menu.read())
+                    view_options()
+            else:
+                user = False
+
+
+        view_options()
+
+    # --------------------------------------- Add/Delete Meal Options ---------------------------------------#
+    elif user == "change":
+        def make_change():
+            change = input("Welcome to the change menu. Would you like to 'view', 'add', or 'delete' a restaurant? "
+                           "\n").lower()
+            # ------------------------------------ View Options ------------------------------------ #
+            if change == "view":
+                option_view = input("Do you want to view the options for 'Breakfast', 'Lunch', or 'Dinner'? \n").lower()
+                if option_view == "breakfast":
+                    with open("breakfast.txt") as b_menu:
+                        print(b_menu.read())
+                        make_change()
+                elif option_view == "lunch":
+                    with open("lunch.txt") as l_menu:
+                        print(l_menu.read())
+                        make_change()
+                elif option_view == "dinner":
+                    with open("dinner.txt") as d_menu:
+                        print(d_menu.read())
+                        make_change()
+
+            # ------------------------------------ Addition Options ------------------------------------ #
+            elif change == "add":
+                def change_list():
+                    global user
+                    option_change = input("Which list would you like to add to? 'Breakfast,' 'Lunch,' or 'Dinner? "
+                                          "\n").lower()
+                    if option_change == "breakfast":
+                        with open("breakfast.txt") as b_menu:
+                            print(f"your current list is: \n {b_menu.read()}")
+                        new_restaurant = input("\n \n What is the name of the restaurant you wish to add? \n")
+                        b_menu = open("breakfast.txt", "a")  # append mode
+                        b_menu.write(f"\n{new_restaurant}")
+                        b_menu.close()
+                        with open("breakfast.txt") as b_menu:
+                            print(f"your updated list is: \n {b_menu.read()}")
+                        change_list()
+                    elif option_change == "lunch":
+                        with open("lunch.txt") as l_menu:
+                            print(f"your current list is: \n {l_menu.read()}")
+                        new_restaurant = input("\n \n What is the name of the restaurant you wish to add? \n")
+                        l_menu = open("lunch.txt", "a")  # append mode
+                        l_menu.write(f"\n{new_restaurant}")
+                        l_menu.close()
+                        with open("lunch.txt") as l_menu:
+                            print(f"your updated list is: \n {l_menu.read()}")
+                        change_list()
+                    elif option_change == "dinner":
+                        with open("dinner.txt") as d_menu:
+                            print(f"your current list is: \n {d_menu.read()}")
+                        new_restaurant = input("\n \n What is the name of the restaurant you wish to add? \n")
+                        d_menu = open("dinner.txt", "a")  # append mode
+                        d_menu.write(f"\n{new_restaurant}")
+                        d_menu.close()
+                        with open("dinner.txt") as d_menu:
+                            print(f"your updated list is: \n {d_menu.read()}")
+                        change_list()
+                    else:
+                        user = False
+                change_list()
+
+            # ------------------------------------ Delete Options ------------------------------------ #
+            elif change == "delete":
+                pass
+                # TODO: Add function for deleting items from lists
+
+        make_change()
+
     else:
-        print("Error in module, please run program again")
+        print("Error, please try again")
         user = False
