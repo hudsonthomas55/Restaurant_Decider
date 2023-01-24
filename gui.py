dd0b42383a04d3610dde9842bc1fab666f77952e
Thomas <112832846+hudsonthomas55@users.noreply.github.com>
2023-01-23 19:20:35 -0500

    Update gui.py
    Fixed bugs with 'back' button from view menu. Merged three view menus into one, rearranged. General code cleanup. Merged main screen into a callable function

--- gui.py
+++ gui.py
@@ -15,69 +15,33 @@ window.config(padx=50, pady=50)
 
 # --------------------------------------------- Functions ---------------------------------------------- #
 def breakfast_choice():
+    clear_frame()
+    main_screen()
     with open("breakfast.txt", 'r') as b_menu:
         restaurants = b_menu.readlines()
         random_rest = random.choice(restaurants)
-    return_label.config(text=f"\n Your restaurant choice is: {random_rest}")
+    return_label = Label(text=f"\n Your restaurant choice is: {random_rest} \n\n", font=("Arial", 24))
+    return_label.grid(row=2, column=0, columnspan=3)
 
 
 def lunch_choice():
+    clear_frame()
+    main_screen()
     with open("lunch.txt", 'r') as l_menu:
         restaurants = l_menu.readlines()
         random_rest = random.choice(restaurants)
-    return_label.config(text=f"\n Your restaurant choice is: {random_rest}")
+    return_label = Label(text=f"\n Your restaurant choice is: {random_rest} \n\n", font=("Arial", 24))
+    return_label.grid(row=2, column=0, columnspan=3)
 
 
 def dinner_choice():
+    clear_frame()
+    main_screen()
     with open("dinner.txt", 'r') as d_menu:
         restaurants = d_menu.readlines()
         random_rest = random.choice(restaurants)
-    return_label.config(text=f"\n Your restaurant choice is: {random_rest}")
-
-
-def view_breakfast():
-    clear_frame()
-    return_number = 1
-    option_list = []
-    with open("breakfast.txt") as b_menu:
-        for option in b_menu.readlines():
-            option_list.append(option)
-    for restaurant in option_list:
-        option_label = Label(text=f"{return_number}. {restaurant}", font=("Arial", 12))
-        option_label.grid(row=return_number, column=0)
-        return_number += 1
-    back_button = Button(text="Go Back", width=15, command=main_screen)
-    back_button.grid(row=0, column=0)
-
-
-def view_lunch():
-    clear_frame()
-    return_number = 1
-    option_list = []
-    with open("lunch.txt") as l_menu:
-        for option in l_menu.readlines():
-            option_list.append(option)
-    for restaurant in option_list:
-        option_label = Label(text=f"{return_number}. {restaurant}", font=("Arial", 12))
-        option_label.grid(row=return_number, column=0)
-        return_number += 1
-    back_button = Button(text="Go Back", width=15, command=main_screen)
-    back_button.grid(row=0, column=0)
-
-
-def view_dinner():
-    clear_frame()
-    return_number = 1
-    option_list = []
-    with open("dinner.txt") as d_menu:
-        for option in d_menu.readlines():
-            option_list.append(option)
-    for restaurant in option_list:
-        option_label = Label(text=f"{return_number}. {restaurant}", font=("Arial", 12))
-        option_label.grid(row=return_number, column=0)
-        return_number += 1
-    back_button = Button(text="Go Back", width=15, command=main_screen)
-    back_button.grid(row=0, column=0)
+    return_label = Label(text=f"\n Your restaurant choice is: {random_rest} \n\n", font=("Arial", 24))
+    return_label.grid(row=2, column=0, columnspan=3)
 
 
 def clear_frame():
@@ -85,63 +49,81 @@ def clear_frame():
         widget.destroy()
 
 
+# ---------------------------------------------- Main Screen ---------------------------------------------- #
 def main_screen():
     clear_frame()
-    new_welcome_label = Label(text="Welcome! \n What meal are you hungry for? \n", font=("Arial", 30))
-    new_welcome_label.grid(row=0, column=0, columnspan=3)
+    welcome_label = Label(text="Welcome! \n What meal are you hungry for? \n", font=("Arial", 30))
+    welcome_label.grid(row=0, column=0, columnspan=3)
 
     # Meal Buttons
-    new_breakfast_button = Button(text="Breakfast", width=15, command=breakfast_choice)
-    new_breakfast_button.grid(row=1, column=0)
-    new_lunch_button = Button(text="Lunch", width=15, command=lunch_choice)
-    new_lunch_button.grid(row=1, column=1)
-    new_dinner_button = Button(text="Dinner", width=15, command=dinner_choice)
-    new_dinner_button.grid(row=1, column=2)
+    breakfast_button = Button(text="Breakfast", width=15, command=breakfast_choice)
+    breakfast_button.grid(row=1, column=0)
+    lunch_button = Button(text="Lunch", width=15, command=lunch_choice)
+    lunch_button.grid(row=1, column=1)
+    dinner_button = Button(text="Dinner", width=15, command=dinner_choice)
+    dinner_button.grid(row=1, column=2)
 
     # Bottom Label(s)
-    new_return_label = Label(text="\nClick a button above to decide pick a restaurant \n\n", font=("Arial", 24))
-    new_return_label.grid(row=2, column=0, columnspan=3)
+    return_label = Label(text="\n\n\n", font=("Arial", 24))
+    return_label.grid(row=2, column=0, columnspan=3)
 
     # add_button = Button(text="Add Restaurant", width=15, command=add_menu)
     # add_button.grid(row=3, column=0)
 
     # View buttons and options
-    new_breakfast_view_button = Button(text="View Breakfast Options", width=15, command=view_breakfast)
-    new_breakfast_view_button.grid(row=3, column=0)
-    new_lunch_view_button = Button(text="View Lunch Options", width=15, command=view_lunch)
-    new_lunch_view_button.grid(row=3, column=1)
-    new_dinner_view_button = Button(text="View Dinner Options", width=15, command=view_dinner)
-    new_dinner_view_button.grid(row=3, column=2)
+    breakfast_view_button = Button(text="View Breakfast Options", width=15, command=view_menu)
+    breakfast_view_button.grid(row=3, column=0)
 
 
-# ---------------------------------------------- Main Screen ---------------------------------------------- #
+# ---------------------------------------------- View Screen ---------------------------------------------- #
+def view_menu():
+    clear_frame()
+    # BACK BUTTON
+    back_button = Button(text="Go Back", width=5, command=main_screen)
+    back_button.grid(row=0, column=0)
+    # HEADER LABEL
+    header_label = Label(text="Welcome to the view menu! See below list of restaurant options: \n", font=("Arial", 30))
+    header_label.grid(row=1, column=0, columnspan=3)
 
-# Top Label(s)
-welcome_label = Label(text="Welcome! \n What meal are you hungry for? \n", font=("Arial", 30))
-welcome_label.grid(row=0, column=0, columnspan=3)
+    # BREAKFAST SECTION
+    breakfast_label = Label(text="Breakfast: \n")
+    breakfast_label.grid(row=2, column=0)
+    return_number = 1
+    breakfast_option_list = []
+    with open("breakfast.txt") as b_menu:
+        for option in b_menu.readlines():
+            breakfast_option_list.append(option)
+    for restaurant in breakfast_option_list:
+        option_label = Label(text=f"{return_number}. {restaurant}", font=("Arial", 12))
+        option_label.grid(row=(return_number + 2), column=0)
+        return_number += 1
 
-# Meal Buttons
-breakfast_button = Button(text="Breakfast", width=15, command=breakfast_choice)
-breakfast_button.grid(row=1, column=0)
-lunch_button = Button(text="Lunch", width=15, command=lunch_choice)
-lunch_button.grid(row=1, column=1)
-dinner_button = Button(text="Dinner", width=15, command=dinner_choice)
-dinner_button.grid(row=1, column=2)
+    # LUNCH SECTION
+    lunch_label = Label(text="Lunch: \n")
+    lunch_label.grid(row=2, column=1)
+    return_number = 1
+    lunch_option_list = []
+    with open("lunch.txt") as l_menu:
+        for option in l_menu.readlines():
+            lunch_option_list.append(option)
+    for restaurant in lunch_option_list:
+        option_label = Label(text=f"{return_number}. {restaurant}", font=("Arial", 12))
+        option_label.grid(row=(return_number + 2), column=1)
+        return_number += 1
 
-# Bottom Label(s)
-return_label = Label(text="\nClick a button above to decide pick a restaurant \n\n", font=("Arial", 24))
-return_label.grid(row=2, column=0, columnspan=3)
+    # DINNER SECTION
+    dinner_label = Label(text="Dinner: \n")
+    dinner_label.grid(row=2, column=2)
+    return_number = 1
+    option_list = []
+    with open("dinner.txt") as d_menu:
+        for option in d_menu.readlines():
+            option_list.append(option)
+    for restaurant in option_list:
+        option_label = Label(text=f"{return_number}. {restaurant}", font=("Arial", 12))
+        option_label.grid(row=(return_number + 2), column=2)
+        return_number += 1
 
-# add_button = Button(text="Add Restaurant", width=15, command=add_menu)
-# add_button.grid(row=3, column=0)
-
-# View buttons and options
-breakfast_view_button = Button(text="View Breakfast Options", width=15, command=view_breakfast)
-breakfast_view_button.grid(row=3, column=0)
-lunch_view_button = Button(text="View Lunch Options", width=15, command=view_lunch)
-lunch_view_button.grid(row=3, column=1)
-dinner_view_button = Button(text="View Dinner Options", width=15, command=view_dinner)
-dinner_view_button.grid(row=3, column=2)
 
 # ---------------------------------------------- Add Screen ---------------------------------------------- #
 # def add_menu():
@@ -163,4 +145,6 @@ dinner_view_button.grid(row=3, column=2)
 
 
 # ---------------------------------------------- KEEP WINDOW OPEN ---------------------------------------------- #
+main_screen()
+
 window.mainloop()
