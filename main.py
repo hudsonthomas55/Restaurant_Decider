81dc327108f1a1c8f2c111ff67883495880fb43c
Thomas <112832846+hudsonthomas55@users.noreply.github.com>
2023-01-24 06:42:30 -0500

    update main.py
    created add screen, add buttons, text entry etc. Submit button not functioning at this time, but set up.

--- main.py
+++ main.py
@@ -75,9 +75,11 @@ def main_screen():
 # ---------------------------------------------- View Screen ---------------------------------------------- #
 def view_menu():
     clear_frame()
-    # BACK BUTTON
+    # TOP BUTTONS
     back_button = Button(text="Go Back", width=5, command=main_screen)
     back_button.grid(row=0, column=0)
+    add_button = Button(text="Add Restaurant", width=10, command=add_menu)
+    add_button.grid(row=0, column=2)
     # HEADER LABEL
     header_label = Label(text="\n Welcome to the view menu! See below list of restaurant options: \n", font=("Arial",
                                                                                                              30))
@@ -124,7 +126,80 @@ def view_menu():
 
 
 # ---------------------------------------------- Add Screen ---------------------------------------------- #
+def add_menu():
+    clear_frame()
+    # TOP BUTTONS
+    back_button = Button(text="Main Menu", width=5, command=main_screen)
+    back_button.grid(row=0, column=0)
+    add_button = Button(text="Add Restaurant", width=10)  # Add command to this line
+    add_button.grid(row=0, column=2)
+    # HEADER LABEL
+    header_label = Label(text="\n Welcome to the add menu! See below list of restaurant options: \n", font=("Arial",
+                                                                                                             30))
+    header_label.grid(row=1, column=0, columnspan=3)
 
+    # BREAKFAST SECTION
+    breakfast_label = Label(text="Breakfast: \n")
+    breakfast_label.grid(row=2, column=0)
+    return_number = 1
+    breakfast_option_list = []
+    with open("breakfast.txt") as b_menu:
+        for option in b_menu.readlines():
+            breakfast_option_list.append(option)
+    for restaurant in breakfast_option_list:
+        option_label = Label(text=f"{return_number}. {restaurant}", font=("Arial", 12))
+        option_label.grid(row=(return_number + 2), column=0)
+        return_number += 1
+
+    # LUNCH SECTION
+    lunch_label = Label(text="Lunch: \n")
+    lunch_label.grid(row=2, column=1)
+    return_number = 1
+    lunch_option_list = []
+    with open("lunch.txt") as l_menu:
+        for option in l_menu.readlines():
+            lunch_option_list.append(option)
+    for restaurant in lunch_option_list:
+        option_label = Label(text=f"{return_number}. {restaurant}", font=("Arial", 12))
+        option_label.grid(row=(return_number + 2), column=1)
+        return_number += 1
+
+    # DINNER SECTION
+    dinner_label = Label(text="Dinner: \n")
+    dinner_label.grid(row=2, column=2)
+    return_number = 1
+    option_list = []
+    with open("dinner.txt") as d_menu:
+        for option in d_menu.readlines():
+            option_list.append(option)
+    for restaurant in option_list:
+        option_label = Label(text=f"{return_number}. {restaurant}", font=("Arial", 12))
+        option_label.grid(row=(return_number + 2), column=2)
+        return_number += 1
+
+    # ADD SECTION
+    add_label = Label(text="\n Add your new restaurant below: \n", font=("Arial",30))
+    add_label.grid(row=399, column=0, columnspan=3)
+    # ENTRY BOX
+    restaurant_label = Label(text="Restaurant Name:")
+    restaurant_label.grid(row=400, column=0)
+    restaurant_name = Entry(width=30)
+    restaurant_name.insert(END, string="Be mindful of spelling and capitalization")
+    restaurant_name.grid(row=400, column=1)
+    # CHECKBOXES
+    check_label = Label(text="This restaurant serves:")
+    check_label.grid(row=401, column=0)
+    b_checkbox = Checkbutton(text="Breakfast")
+    b_checkbox.grid(row=402, column=0)
+    l_checkbox = Checkbutton(text="Lunch")
+    l_checkbox.grid(row=402, column=1)
+    d_checkbox = Checkbutton(text="Dinner")
+    d_checkbox.grid(row=402, column=2)
+    # SUBMIT BUTTON
+    submit_button = Button(text="Submit", width=15)      # ADD COMMAND
+    submit_button.grid(row=403, column=1)
+    # TODO create command tying the submit button to entry label
+        # Use "get"?
 
 
 # ---------------------------------------------- KEEP WINDOW OPEN ---------------------------------------------- #
